b58247d890ec89caefc6e40df857f950ff359b95
hamza  alaya <alaya.hamza1@gmail.com>
2019-12-31 03:11:35 +0100

    Updates

---
+++ precache-manifest.7237ee00b0d75a01a269479621511353.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5a175eead8029b4fb0393da6e039db4",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "c4fb186f46dc4e7a8fb9",
+    "url": "/todolist/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "c4fb186f46dc4e7a8fb9",
+    "url": "/todolist/static/js/2.8c63ad5f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/todolist/static/js/2.8c63ad5f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d16de535cc1257869f25",
+    "url": "/todolist/static/js/main.ec18baf8.chunk.js"
+  },
+  {
+    "revision": "507716673e279f58353e",
+    "url": "/todolist/static/js/runtime-main.948c6c71.js"
+  }
+]);
